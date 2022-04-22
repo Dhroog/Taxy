b0b428c5887f573662dc1c9a7ae08660509ba1d9
unknown <yousefdhrog@gmail.com>
2022-04-22 23:19:15 +0300

    three commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,11 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
+
+
 class UserController extends Controller
 {
+
     use GeneralTrait;
     public function role(): JsonResponse
     {
