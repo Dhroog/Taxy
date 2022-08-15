1aafd80e15136bed853744fee3d5359e6e816236
ASUS <yousefdhrog@gmail.com>
2022-08-15 20:10:47 +0300

    pagination

--- routes/api.php
+++ routes/api.php
@@ -163,7 +163,7 @@ Route::group(["prefix" => "Trip", "middleware" => ["auth:sanctum", "ActiveAccoun
     Route::get("GetDriverTrips/{id}", [TripController::class, "GetDriverTrips"])->middleware("permission:Get-Driver-Trips");
     Route::get("AcceptTrip/{id}", [TripController::class, "AcceptTrip"])->middleware("permission:Accept-Trip");
     Route::post("SendCancellationReason", [CancellationReasonController::class, "SendCancellationReason"])->middleware("permission:Send-Cancellation-Reason");
-    Route::post("SetTrackingTrip/{id}", [TripController::class, "SetTrackingTrip"])->middleware("permission:Set-Tracking-Trip");
+    Route::post("SetTrackingTrip", [TripController::class, "SetTrackingTrip"])->middleware("permission:Set-Tracking-Trip");
     Route::post('SendRate',[TripController::class,'SendRate'])->middleware("permission:Send-Rate");
     Route::get("GetMyActiveTrip", [TripController::class, "GetMyActiveTrip"])->middleware("permission:Get-My-Active-Trip");
     Route::get("GetReasonsCancellationForTrip/{id}", [TripController::class, "GetReasonsCancellationForTrip"])->middleware("permission:Get-Reasons-Cancellation-For-Trip");
